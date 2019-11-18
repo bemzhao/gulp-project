08910b303f99a61c7d02e835419554329695d2d9
ZhaoShaofeng <bem.zhao@foxmail.com>
2019-11-19 02:00:08 +0800

    1、压缩合并 css、js 生成 sourcemaps 和 hash 2、压缩 images 3、根据 hash 替换 html 中的文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const { src, dest, series } = require('gulp');
 const clean = require('gulp-clean');
-const htmlmin = require('gulp-htmlmin');
-const cleanCss = require('gulp-clean-css');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
+      htmlmin = require('gulp-htmlmin');
+      cleanCss = require('gulp-clean-css');
+      sourcemaps = require('gulp-sourcemaps');
+      autoprefixer = require('gulp-autoprefixer');
+      concat = require('gulp-concat');
+      uglify = require('gulp-uglify');
+      rev = require('gulp-rev');
+      revReplace = require("gulp-rev-replace");
+      imagemin = require('gulp-imagemin');
+      babel = require('gulp-babel');
 
 
 // 清空 dist
@@ -13,29 +18,45 @@ function cleanFiles () {
 		.pipe(clean());
 }
 
-// 压缩 css
+// 压缩合并 css  生成 sourcemaps 和 hash 
 function minifyCss() {
 	return src('src/css/*.css')
 		.pipe(sourcemaps.init())
 		.pipe(autoprefixer())
     .pipe(cleanCss())
+    // .pipe(concat('index.min.css'))
+    .pipe(rev())
     .pipe(sourcemaps.write('.'))
-    .pipe(dest('dist/css'));
+    .pipe(dest('dist/css'))
+    .pipe(rev.manifest())
+		.pipe(dest('dist/rev/css'));
 }
 
-function concatCss () {
-	return src('src/*.css', {
-			ignore: [
-				'src/css/bootstrap.min.css',
-				'src/css/swiper.min.css',
-				'src/css/animate.min.css',
-			]
-		})
-		.pipe(concat('index.min.js'))
-		.pipe(dest('dist/css'));
+// 压缩合并 js  生成 sourcemaps 和 hash 
+function minifyJs () {
+	return src('src/js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+		.pipe(sourcemaps.init())
+		.pipe(uglify())
+		// .pipe(concat('index.min.js'))
+		.pipe(rev())
+		.pipe(sourcemaps.write('.'))
+		.pipe(dest('dist/js'))
+		.pipe(rev.manifest())
+		.pipe(dest('dist/rev/js'));
 }
 
-// 压缩 html
+function minifyImg() {
+	return src(['src/images/*', 'src/images/**/*'])
+		.pipe(imagemin({
+			progressive: true
+		}))
+		.pipe(dest('dist/images'))
+}
+
+// 压缩 html 生成 hash 
 function minifyHtml () {
 	return src('src/*.html')
     .pipe(htmlmin({ 
@@ -46,8 +67,17 @@ function minifyHtml () {
     	minifyCSS: true,
     	minifyJS: true 
     }))
+    .pipe(rev())
     .pipe(dest('dist'));
 }
 
-exports.build = series(cleanFiles, minifyCss, concatCss);
-exports.default = concatCss;
+// 根据 hash 替换 html 中的文件
+function revreplace () {
+	var manifest = src('dist/rev/**/rev-manifest.json');
+	return src('src/*.html')
+    .pipe(revReplace({manifest: manifest}))
+    .pipe(dest('dist'));
+}
+
+exports.build = series(cleanFiles, minifyCss, minifyJs, minifyImg, revreplace);
+exports.default = minifyJs;
